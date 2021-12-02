d01d8cc1b9f54b06f63c44da2238b99c5dcda76a
2521250151 <2521250151@qq.com>
2021-12-02 18:25:19 +0800

    JDBC的增删改

--- src/com/jdbc/day01/Test2.java
+++ src/com/jdbc/day01/Test2.java
@@ -12,7 +12,7 @@ import java.sql.PreparedStatement;
  * @DateTime: 2021/11/27 9:58
  */
 public class Test2 {
-    Student stu=new Student(66,"现在是",18,"北京");
+    Student stu=new Student(7,"现在是",18,"北京");
     @Test
     public void test1() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -31,5 +31,28 @@ public class Test2 {
     }
     @Test
     public void test2() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe?","root","1012");
+        String sql = "update student set sname=?,sage=?,saddress=? where id=?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setObject(1,stu.getName());
+        ps.setObject(2, stu.getAge());
+        ps.setObject(3, stu.getAddress());
+        ps.setObject(4, stu.getId());
+        int i = ps.executeUpdate();
+        System.out.println(i);
+        conn.close();
+        ps.close();
+    }
+    @Test
+    public void test3() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe", "root", "1012");
+        PreparedStatement ps = conn.prepareStatement("delete from  student where id=?");
+        ps.setObject(1,stu.getId());
+        int i = ps.executeUpdate();
+        System.out.println(i);
+        conn.close();
+        ps.close();
     }
 }
