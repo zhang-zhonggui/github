76a110da548e14fed8737ea0fa1b8b12de60e7b1
2521250151 <2521250151@qq.com>
2021-12-03 15:45:06 +0800

    查询数据库中的数据,课堂上的讲得

--- src/com/jdbc/day02/classroom/GredeTest.java
+++ src/com/jdbc/day02/classroom/GredeTest.java
@@ -19,16 +19,20 @@ public class GredeTest {
         //输入查询语句
         PreparedStatement ps = conn.prepareStatement("select * from class");
         ResultSet set = ps.executeQuery();
+        //list在外面，在里面会被覆盖
         List<Grede> list = new LinkedList<>();
         while (set.next()) {
+            //在里面创建对象，每进行一次，就会重新创建对象然后进行存储
             Grede grede = new Grede();
             grede.setId(set.getInt(1));
             grede.setGrede(set.getString(2));
             list.add(grede);
         }
         conn.close();
+        //循环遍历输出
         for (Grede a:list) {
             System.out.println(a.getId()+"======="+a.getGrede());
+
         }
     }
 }
