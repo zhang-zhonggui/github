20b7c1b87d01edbf2e69a6af63e4ac257059e70a
2521250151 <2521250151@qq.com>
2021-12-03 14:35:35 +0800

    查询数据库中的数据

--- src/com/jdbc/day02/classroom/Select.java
+++ src/com/jdbc/day02/classroom/Select.java
@@ -2,12 +2,7 @@ package com.jdbc.day02.classroom;
 
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import static sun.misc.MessageUtils.where;
+import java.sql.*;
 
 /**
  * @Author: zzg
@@ -16,26 +11,38 @@ import static sun.misc.MessageUtils.where;
  */
 public class Select {
     @Test
-    public void test1() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe", "root", "1012");
-        //输入查询语句
-        PreparedStatement ps = conn.prepareStatement("select * from student");
-        ResultSet set = ps.executeQuery();
-        //通过移动光标判断有没有下一个
-        while(set.next()){
-            //通过id获取set中的值
-            Object id = set.getObject(1);
-            //通过表名获取值
-            Object name = set.getObject("sname");
-            int age = set.getInt("sage");
-            Object address = set.getObject(4);
-            System.out.println(id+"==="+name+"==="+age+"==="+address);
+    public void test1() {
+        Connection conn = null;
+        //异常处理
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe", "root", "1012");
+            //输入查询语句
+            PreparedStatement ps = conn.prepareStatement("select * from student");
+            ResultSet set = ps.executeQuery();
+            //通过移动光标判断有没有下一个
+            while (set.next()) {
+                //通过id获取set中的值
+                Object id = set.getObject(1);
+                //通过表名获取值
+                Object name = set.getObject("sname");
+                int age = set.getInt("sage");
+                Object address = set.getObject(4);
+                System.out.println(id + "===" + name + "===" + age + "===" + address);
+            }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    //关闭资源
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        //关闭资源
-        set.close();
-        ps.close();
-        conn.close();
+
     }
 }
