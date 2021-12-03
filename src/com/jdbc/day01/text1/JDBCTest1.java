da37b85a1a1a9d82eb27f7b488260f0932d3c45a
2521250151 <2521250151@qq.com>
2021-12-03 08:48:05 +0800

    JDBC的查询的练习

---
+++ src/com/jdbc/day01/text1/JDBCTest1.java
@@ -0,0 +1,40 @@
+package com.jdbc.day01.text1;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/**
+ * @Author: zzg
+ * @Description: jdbc的一次联系
+ * @DateTime: 2021/11/27 9:58
+ */
+public class JDBCTest1 {
+    @Test
+    public void test1() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe", "root", "1012");
+        PreparedStatement ps = conn.prepareStatement("insert into student values(null,'张三',45,'北京')");
+        int i = ps.executeUpdate();
+        System.out.println(1);
+    }
+    @Test
+    public void test2() throws Exception {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe", "root", "1012");
+        PreparedStatement ps = conn.prepareStatement("delete from student where id = 6");
+        int i = ps.executeUpdate();
+        System.out.println(i);
+    }
+    @Test
+    public void test3() throws Exception{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hehe", "root", "1012");
+        PreparedStatement ps = conn.prepareStatement("update student set sname='张三' where id=1 ");
+        int i = ps.executeUpdate();
+        System.out.println(i);
+    }
+}
